34f54632cbcbabba219d583e1cd71f01de6ef2bc
Jovin Joju <jovinjoju1999@gmail.com>
2024-07-12 02:19:50 +0530

    Updated mainActivity.kt
    Added 4 required states

--- app/src/main/java/eu/tutorials/timerapp/MainActivity.kt
+++ app/src/main/java/eu/tutorials/timerapp/MainActivity.kt
@@ -3,13 +3,24 @@ package eu.tutorials.timerapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
 import eu.tutorials.timerapp.ui.theme.TimerAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,4 +30,34 @@ class MainActivity : ComponentActivity() {
 
         }
     }
+}
+
+
+@Composable
+fun Timer (
+    totalTime: Long,
+    handleColor: Color,
+    inactiveBarColor: Color,
+    activeBarColor: Color,
+    modifier: Modifier = Modifier,
+    initialValue: Float = 0f,
+    strokeWidth: Dp = 5.dp
+){
+    var size by remember {
+        mutableStateOf(IntSize.Zero)
+    }
+
+    var value by remember {
+        mutableStateOf(initialValue)
+    }
+
+    var currentTime by remember {
+        mutableStateOf(totalTime)
+    }
+
+    var isTimerRunning by remember {
+        mutableStateOf(false)
+    }
+
+    
 }
