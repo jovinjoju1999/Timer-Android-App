1a1ac53a40e050447b0f523d99be7715e833d809
Jovin Joju <jovinjoju1999@gmail.com>
2024-07-12 02:43:13 +0530

    Update MainActivity.kt(Timer Curve design)
    Added timer curve designs and corresponding math functions.

--- app/src/main/java/eu/tutorials/timerapp/MainActivity.kt
+++ app/src/main/java/eu/tutorials/timerapp/MainActivity.kt
@@ -3,6 +3,7 @@ package eu.tutorials.timerapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -15,13 +16,21 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PointMode
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import eu.tutorials.timerapp.ui.theme.TimerAppTheme
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -59,5 +68,42 @@ fun Timer (
         mutableStateOf(false)
     }
 
-    
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.onSizeChanged {
+            size=it
+        }
+    ){
+        Canvas(modifier = modifier){
+            drawArc(
+                color=inactiveBarColor,
+                startAngle = -215f,
+                sweepAngle = 250f,
+                useCenter = false,
+                size = Size(size.width.toFloat(),size.height.toFloat()),
+                style=Stroke(strokeWidth.toPx(), cap= StrokeCap.Round)
+            )
+            drawArc(
+                color=activeBarColor,
+                startAngle = -215f,
+                sweepAngle = 250f * value,
+                useCenter = false,
+                size = Size(size.width.toFloat(),size.height.toFloat()),
+                style=Stroke(strokeWidth.toPx(), cap= StrokeCap.Round)
+            )
+
+            val center = Offset(size.width/2f, size.height/2f)
+            val beta = (250f * value + 145f) * (PI/180f).toFloat()
+            val r = size.width / 2f
+            val a = cos(beta) * r
+            val b = sin(beta) * r
+            drawPoints(
+                listOf(Offset(center.x+a, center.y+b)),
+                pointMode = PointMode.Points,
+                color = handleColor,
+                strokeWidth = (strokeWidth * 3f).toPx(),
+                cap = StrokeCap.Round
+            )
+        }
+    }
 }
